63fb77702200fa4dc684880d804dd330fdaa8a13
Evgeny Korobenkov <joekor463@gmail.com>
2020-05-08 22:05:24 +0800

    float and double

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.timbuchalka;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " +  myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+        double myPound = 36f;
+        double myKg = myPound * 0.45359237;
+        System.out.println("MyKg = " + myKg);
+    }
+}
